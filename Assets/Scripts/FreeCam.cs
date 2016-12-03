beeedf2473a33ce9700861773ccdec05410e025b
Phyronnaz <phyronnaz@gmail.com>
2016-12-03 14:44:46 +0100

    Camera fix

--- Assets/Scripts/FreeCam.cs
+++ Assets/Scripts/FreeCam.cs
@@ -23,9 +23,7 @@ public class FreeCam : MonoBehaviour
 
     private void Update()
     {
-        Vector3 deltaPosition = Vector3.zero;
-
-        deltaPosition += transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+        var deltaPosition = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
 
         var currentSpeed = speed;
         if (Input.GetKey(sprintButton))
@@ -38,10 +36,10 @@ public class FreeCam : MonoBehaviour
 
         if (!togglePressed)
         {
-            Vector3 eulerAngles = transform.eulerAngles;
+            Vector3 eulerAngles = Vector3.zero;
             eulerAngles.x += -Input.GetAxis("Mouse Y") * 359f * cursorSensitivity;
             eulerAngles.y += Input.GetAxis("Mouse X") * 359f * cursorSensitivity;
-            transform.eulerAngles = eulerAngles;
+            transform.Rotate(eulerAngles);
         }
 
 
